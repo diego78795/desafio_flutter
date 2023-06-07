50dd4397192f81a9d6b7e50fb98496826e6e4383
Diego Camargo <diego.cruz.camargo@gmail.com>
2023-06-07 19:31:51 -0300

    feat: Passando repositorio de movie para o controlador da Home Page pela rota

--- lib/app/bindings/home_binding.dart
+++ lib/app/bindings/home_binding.dart
@@ -1,9 +1,15 @@
-import 'package:desafio_flutter/app/controllers/home_controller.dart';
 import 'package:get/get.dart';
+import 'package:http/http.dart' as http;
+
+import 'package:desafio_flutter/app/controllers/home_controller.dart';
+import 'package:desafio_flutter/app/data/providers/api.dart';
+import 'package:desafio_flutter/app/data/repository/movie_repository.dart';
 
 class HomeBinding extends Bindings {
   @override
   void dependencies() {
-    Get.put(HomeController());
+    Get.put(HomeController(
+        movieRepository:
+            MovieRepository(apiClient: ApiClient(httpClient: http.Client()))));
   }
 }
