b68837626f857dae3f151ce9633b49315646908c
Diego Camargo <diego.cruz.camargo@gmail.com>
2023-06-07 19:34:30 -0300

    feat: Implementando getGenreMovies no controller da Home Page

--- lib/app/controllers/home_controller.dart
+++ lib/app/controllers/home_controller.dart
@@ -1,7 +1,30 @@
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
+import 'package:desafio_flutter/app/data/model/movie_model.dart';
+import 'package:desafio_flutter/app/data/repository/movie_repository.dart';
+
 class HomeController extends GetxController {
+  final MovieRepository? movieRepository;
+  HomeController({@required this.movieRepository})
+      : assert(movieRepository != null);
 
-HomeController();
+  bool isLoading = true;
+  List<MovieModel> movieList = [];
 
+  @override
+  void onInit() {
+    fetchData();
+    super.onInit();
+  }
+
+  Future<void> fetchData() async {
+    movieList = await getGenreMovies(28);
+    isLoading = false;
+    update();
+  }
+
+  Future<List<MovieModel>> getGenreMovies(int idGenre) async {
+    return await movieRepository?.getGenreMovies(idGenre);
+  }
 }
