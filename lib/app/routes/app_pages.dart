860fbca3a32bd00a4d2935329a603a329daf208d
Diego Camargo <diego.cruz.camargo@gmail.com>
2023-06-07 16:37:41 -0300

    feat: Implementando binding da Home Page na rota

--- lib/app/routes/app_pages.dart
+++ lib/app/routes/app_pages.dart
@@ -1,11 +1,12 @@
+import 'package:desafio_flutter/app/bindings/home_binding.dart';
 import 'package:desafio_flutter/app/ui/home/home_page.dart';
 import 'package:get/get.dart';
 
 part './app_routes.dart';
 
 abstract class AppPages {
-
   static final pages = [
-    GetPage(name: Routes.HOME, page:()=> const HomePage(),)
+    GetPage(
+        name: Routes.HOME, page: () => const HomePage(), binding: HomeBinding())
   ];
 }
