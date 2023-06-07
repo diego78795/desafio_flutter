cc03ac62bafefb6cec9608ea44582a5e91e95d3b
Diego Camargo <diego.cruz.camargo@gmail.com>
2023-06-07 19:29:12 -0300

    feat: Criando modelo de card do filme

---
+++ lib/app/data/model/movie_model.dart
@@ -0,0 +1,24 @@
+class MovieModel {
+  int id = 0;
+  String img = '';
+  String title = '';
+  List<dynamic> genres = [];
+
+  MovieModel();
+
+  MovieModel.fromJson(Map<String, dynamic> json) {
+    id = json['id'];
+    img = json['poster_path'];
+    title = json['title'];
+    genres = json['genre_ids'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = <String, dynamic>{};
+    data['id'] = id;
+    data['img'] = img;
+    data['title'] = title;
+    data['genres'] = genres;
+    return data;
+  }
+}
