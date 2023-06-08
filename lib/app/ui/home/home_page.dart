a6984d5f57cadbf7a02e1897dff5d8be83ad91a2
Diego Camargo <diego.cruz.camargo@gmail.com>
2023-06-08 20:12:15 -0300

    feat: Criando card e renderizando as respostas da api

--- lib/app/ui/home/home_page.dart
+++ lib/app/ui/home/home_page.dart
@@ -1,17 +1,86 @@
-import 'package:desafio_flutter/app/controllers/home_controller.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+
+import 'package:desafio_flutter/app/data/model/movie_model.dart';
+import 'package:desafio_flutter/app/controllers/home_controller.dart';
 
 class HomePage extends GetView<HomeController> {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    return Scaffold(body: GetBuilder<HomeController>(builder: (_) {
+      return SafeArea(
+          child: Center(
+              child: SizedBox(
+                  width: 320,
+                  child: ListView.separated(
+                    itemCount: _.movieList.length,
+                    separatorBuilder: (context, index) {
+                      return const SizedBox(height: 16);
+                    },
+                    itemBuilder: (context, index) {
+                      return CardMovie(movie: _.movieList[index]);
+                    },
+                  ))));
+    }));
+  }
+}
 
-    return const Scaffold(
+class CardMovie extends StatelessWidget {
+  const CardMovie({super.key, required this.movie});
 
-    body: SafeArea(
-      child: Text('Home Page'))
-    );
+  final MovieModel movie;
+
+  @override
+  Widget build(BuildContext context) {
+    debugPrint('$movie');
+    return Stack(children: [
+      ShaderMask(
+        shaderCallback: (rect) {
+          return const LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [Colors.white, Colors.black],
+          ).createShader(Rect.fromLTRB(0, 0, rect.width, rect.height));
+        },
+        child: FittedBox(
+            child: ClipRRect(
+                borderRadius: BorderRadius.circular(50.0),
+                child: Image.network(
+                  'https://image.tmdb.org/t/p/w300${movie.img}',
+                  height: 470.0,
+                  width: 320.0,
+                ))),
+      ),
+      Positioned(
+          top: 397,
+          left: 24,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                movie.title,
+                style: const TextStyle(
+                  color: Colors.white,
+                  fontFamily: 'Montserrat',
+                  fontWeight: FontWeight.w600,
+                  fontSize: 14,
+                ),
+              ),
+              const SizedBox(height: 9),
+              const Text(
+                'Ação - Aventura',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontFamily: 'Montserrat',
+                  fontWeight: FontWeight.w400,
+                  fontSize: 10,
+                ),
+              )
+            ],
+          ))
+    ]);
   }
 }
