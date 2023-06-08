42566abbbb52680d6840c642fd09cc69300c150f
Diego Camargo <diego.cruz.camargo@gmail.com>
2023-06-08 20:35:14 -0300

    feat: Implementando rota que busca lista de generos

--- lib/app/data/providers/api.dart
+++ lib/app/data/providers/api.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
 import 'package:desafio_flutter/app/data/model/movie_model.dart';
+import 'package:desafio_flutter/app/data/model/genres_model.dart';
 
 const baseUrl = "https://api.themoviedb.org/3";
 
@@ -23,6 +24,24 @@ class ApiClient {
             .toList();
       } else {
         debugPrint('Error -apiGenreMovies$idGenre');
+      }
+    } catch (e) {
+      debugPrint('Error fetching from API $e');
+    }
+    return [];
+  }
+
+  Future<List<GenresModel>> apiGenresList() async {
+    try {
+      final response = await httpClient!.get(Uri.parse(
+          '$baseUrl/genre/movie/list?language=pt-BR&api_key=$apiKey'));
+      if (response.statusCode == 200) {
+        List<dynamic> jsonResponse = jsonDecode(response.body)["genres"];
+        return jsonResponse
+            .map((movieJson) => GenresModel.fromJson(movieJson))
+            .toList();
+      } else {
+        debugPrint('Error -apiGenresList');
       }
     } catch (e) {
       debugPrint('Error fetching from API $e');
