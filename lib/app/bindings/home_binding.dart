fbed35a9959387367bf8bb87fde08767b8f5d10b
Diego Camargo <diego.cruz.camargo@gmail.com>
2023-06-08 20:36:34 -0300

    feat: Passando repositorio de generos para Home Controller

--- lib/app/bindings/home_binding.dart
+++ lib/app/bindings/home_binding.dart
@@ -4,12 +4,15 @@ import 'package:http/http.dart' as http;
 import 'package:desafio_flutter/app/controllers/home_controller.dart';
 import 'package:desafio_flutter/app/data/providers/api.dart';
 import 'package:desafio_flutter/app/data/repository/movie_repository.dart';
+import 'package:desafio_flutter/app/data/repository/genres_repository.dart';
 
 class HomeBinding extends Bindings {
   @override
   void dependencies() {
     Get.put(HomeController(
         movieRepository:
-            MovieRepository(apiClient: ApiClient(httpClient: http.Client()))));
+            MovieRepository(apiClient: ApiClient(httpClient: http.Client())),
+        genresRepository:
+            GenresRepository(apiClient: ApiClient(httpClient: http.Client()))));
   }
 }
